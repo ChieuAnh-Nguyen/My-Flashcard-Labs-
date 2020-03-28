bd8396f237ac71ff7f36719d89de2d5e37c90c75
ChieuAnh-Nguyen <chieuanh.nguyen@sjsu.edu>
2020-03-27 22:38:57 -0700

    Final Lab! I love iOS! What a rush!

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
-    @IBOutlet weak var card: UILabel!
-
+    @IBOutlet weak var card: UIView!
+    
     // Array to hold our flashcards
     var flashcards = [Flashcard] ()
     var currentIndex = 0
@@ -27,8 +27,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         card.layer.cornerRadius = 20
-        card.layer.shadowRadius = 15
-        card.layer.shadowOpacity = 5
+        card.layer.shadowRadius = 10
+        card.layer.shadowOpacity = 0.5
         
         frontLabel.layer.cornerRadius = 20
         frontLabel.clipsToBounds = true
@@ -45,21 +45,53 @@ class ViewController: UIViewController {
         }
         
     }
-
-    @IBAction func didTapOnPrev(_ sender: Any) {
+    func deleteCurrentFlashcard(){
+        flashcards.remove(at: currentIndex)
         
+        if currentIndex>flashcards.count - 1{
+        currentIndex = flashcards.count - 1
+        }
+        updateNextPrevButtons()
+        updateLabels()
+        saveAllFlashcardsToDisk()
+    }
+    @IBAction func didTapOnDelete(_ sender: Any) {
+        if currentIndex < 0 {
+               currentIndex = 0
+        }
+        if currentIndex == 0{
+            let alert = UIAlertController(title: "Delete Disabled" , message: "You cannot delete this flashcard", preferredStyle: .actionSheet)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+                   alert.addAction(cancelAction)
+        }
+        else{
+       let alert = UIAlertController(title: "Delete Flashcard", message: "Are you sure you want to delete this flashcard?", preferredStyle:.actionSheet)
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive)
+        { action in
+            self.deleteCurrentFlashcard()
+        }
+        alert.addAction(deleteAction)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancelAction)
+    
+        present(alert, animated: true, completion: nil)
+        
+        }
+    }
+    @IBAction func didTapOnPrev(_ sender: Any) {
+        animateCardOut()
         currentIndex = currentIndex - 1
     
-        if currentIndex < -1 {
+        if currentIndex <= -1 {
             currentIndex = 0
         }
 
-        updateLabels()
+        //updateLabels()
         updateNextPrevButtons()
     }
     
     @IBAction func didTapOnNext(_ sender: Any) {
-        
+        animateCardOut()
         currentIndex = currentIndex + 1
         
         // check if we're out of range
@@ -67,7 +99,7 @@ class ViewController: UIViewController {
             currentIndex = flashcards.count - 1
         }
         
-        updateLabels()
+       // updateLabels()
         updateNextPrevButtons()
     }
     
@@ -80,20 +112,47 @@ class ViewController: UIViewController {
         backLabel.text = currentFlashcard.Answer
     }
     
+   func animateCardIn() {
+    card.transform = CGAffineTransform.identity.translatedBy(x: 300, y: 0.0)
+    UIView.animate(withDuration: 0.3, animations: {
+        self.card.transform = CGAffineTransform.identity
+    })
+    }
+    
+    func animateCardOut() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.card.transform = CGAffineTransform.identity.translatedBy(x: -300, y: 0.0)
+        }, completion: {finished in
+            self.updateLabels()
+            self.animateCardIn()
+            
+            
+        }  )
+    }
     
     @IBOutlet weak var prevButton: UIButton!
     
     @IBOutlet weak var nextButton: UIButton!
     
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        if frontLabel.isHidden {
-            frontLabel.isHidden = false
-            backLabel.isHidden = true
-        } else {
-            frontLabel.isHidden = true
-            backLabel.isHidden = false
-        }
+        flipFlashcard()
     }
+    func flipFlashcard(){
+        UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromRight, animations: {
+            
+            if self.frontLabel.isHidden {
+                self.frontLabel.isHidden = false
+            }
+                
+            else {
+                self.frontLabel.isHidden = true
+
+            }
+        })
+        
+
+    }
+    
     
     func updateFlashcard(question: String, answer: String) {
         let flashcard = Flashcard(Question: question, Answer: answer)
@@ -154,3 +213,5 @@ class ViewController: UIViewController {
                 flashcards.append(contentsOf: savedCards)}
     }
 }
+
+
